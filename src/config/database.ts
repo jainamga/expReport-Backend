6e0241bdb67ed56b2a14ed082f3d7cdeffd52952
Your New Name <jainamga@gmail.com>
2025-07-03 12:08:31 -0400

    env variables edited in database.ts

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,10 +2,10 @@
 import { Sequelize } from 'sequelize';
 
 // Replace with your actual database credentials
-const dbName = 'expenseTracker';
-const dbUser = 'root';
-const dbPass = 'Yell0_BeE!';
-const dbHost = 'localhost';
+const dbName = process.env.DB_NAME || 'expenseTracker';
+const dbUser = process.env.DB_USER || 'root';
+const dbPass = process.env.DB_PASS || 'Yell0_BeE!';
+const dbHost = process.env.DB_HOST || 'localhost';
 
 const sequelize = new Sequelize(dbName, dbUser, dbPass, {
   host: dbHost,
